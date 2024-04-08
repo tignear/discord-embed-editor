790f221dd0210075f2b518b31dec5c0a811523f0
tignear <tignear+m@gmail.com>
2024-04-08 17:40:11 +0900

    feat: edit webhook message

--- src/lib/editor/editor-webhook.svelte
+++ src/lib/editor/editor-webhook.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { DiscordFileData } from '$lib';
 	import Button from '@smui/button';
+	import CircularProgress from '@smui/circular-progress';
 	import IconButton from '@smui/icon-button';
 	import Snackbar, { Actions, Label } from '@smui/snackbar';
 	import Textfield from '@smui/textfield';
@@ -11,11 +12,27 @@
 	export let username: string | undefined;
 	export let icon: string = '';
 	export let files: DiscordFileData[];
+	export let message_id: string = '';
 	let message = '';
 	let sending = false;
 	let snackbar: Snackbar;
 	async function send() {
 		const body = new FormData();
+		const url = new URL(webhook_url);
+		let method;
+		if (message_id !== '') {
+			if (/\d{17,19}/.test(message_id)) {
+				url.pathname += '/messages/' + message_id;
+				method = 'PATCH';
+			} else {
+				message = 'メッセージIDは17-19文字の数字である必要があります。';
+				return;
+			}
+		} else {
+			method = 'POST';
+			url.searchParams.set('wait', 'true');
+		}
+		console.log(url);
 		body.set(
 			'payload_json',
 			JSON.stringify({
@@ -37,12 +54,14 @@
 			body.set(`files[${idx}]`, file.file);
 			++idx;
 		}
-		const resp = await fetch(webhook_url, {
+		const resp = await fetch(url, {
 			body,
-			method: 'POST'
+			method
 		});
-		if (resp.status === 200 || resp.status === 204) {
+		if (resp.status === 200) {
 			message = resp.status + ': 送信しました';
+			const d = await resp.json();
+			message_id = d.id;
 			snackbar.open();
 		} else {
 			const err = await resp.json();
@@ -55,6 +74,7 @@
 
 <div>
 	<Textfield bind:value={webhook_url} label="Webhook URL" style="width: 100%;" required></Textfield>
+	<Textfield bind:value={message_id} label="Message Id" style="width: 100%"></Textfield>
 	<Textfield bind:value={username} label="Username" style="width: 100%" input$emptyValueUndefined
 	></Textfield>
 	<Textfield bind:value={icon} label="Avatar URL" style="width: 100%"></Textfield>
@@ -66,6 +86,9 @@
 		</Actions>
 	</Snackbar>
 	<div style="display: flex; justify-content: flex-end; margin: 4px;">
+		{#if sending}
+			<CircularProgress style="height: 32px; width: 32px;" indeterminate />
+		{/if}
 		<Button
 			disabled={sending || webhook_url == ''}
 			on:click={async () => {
@@ -74,7 +97,7 @@
 				}
 				sending = true;
 				try {
-					send();
+					await send();
 				} finally {
 					sending = false;
 				}
