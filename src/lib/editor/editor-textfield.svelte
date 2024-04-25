51ecc4b521903e7f314b550577d9453f05639861
tignear <tignear+m@gmail.com>
2024-04-26 08:06:45 +0900

    fix: fixed freeze when deleting embed

--- src/lib/editor/editor-textfield.svelte
+++ src/lib/editor/editor-textfield.svelte
@@ -10,7 +10,7 @@
 
 <div class="editor-textfield-wrapper">
 	{#if maxLength == null}
-		<Textfield {textarea} bind:value {label} class="editor-textfield" type={ty}></Textfield>
+		<Textfield {textarea} bind:value {label} class="editor-textfield" type={ty} input$emptyValueUndefined></Textfield>
 	{:else}
 		<Textfield
 			{textarea}
@@ -18,6 +18,7 @@
 			{label}
 			class="editor-textfield"
 			input$maxlength={maxLength}
+			input$emptyValueUndefined
 			type={ty}
 		>
 			<CharacterCounter slot="helper">0 / {maxLength}</CharacterCounter>
